f83265d20684d3a13d4089b372f108257a1ef918
kurepark <eunhye0406@gmail.com>
2018-11-11 02:38:44 +0900

    ajax를 사용해 서브 작업페이지 불러오기 추가

--- work/js/work.js
+++ work/js/work.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
+   $("#workItem").load('desc_kofic.html');
 });//jqb
 
 //kofic불러오기
 function btnKoficPage(){
   $.ajax({ 
-    type: 'post' 
-    , url: '/desc_kofic.html' 
+    type: 'get' 
+    , url: 'desc_kofic.html' 
     , dataType : 'html' 
     , success: function(data) {
-      //$("#workItem").html(data); 
-      console.log(111)
+      $("#workItem").html(data); 
     } 
   });	
+}
+
+function btnArenaPage(){
+  $.ajax({ 
+    type: 'get' 
+    , url: 'desc_arena.html' 
+    , dataType : 'html' 
+    , success: function(data) {
+      $("#workItem").html(data); 
+    } 
+  });	 
 }
