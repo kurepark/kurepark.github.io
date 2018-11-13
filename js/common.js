f2861f591ed531e12ee53d1757f82db6884cba08
kurepark <eunhye0406@gmail.com>
2018-11-13 23:17:58 +0900

    스크립트 일부수정

--- js/common.js
+++ js/common.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-  $('nav.gnb li').on('click',function(){
-    var $this = $(this);
-    $this.addClass('active');
-    $('nav.gnb li').not($this).removeClass('active');
-
-  });
 
   $('.item_slider').slick({
     dots: true,
